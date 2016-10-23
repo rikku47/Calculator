4ef81ef8f74b52f0ad949ea5d41e769030971bec
RacOO <RacOO@DESKTOP-F55NJI8>
2016-10-23 19:41:00 +0200

    Change Assemblyname and Namespace to Calculator

--- WpfApplication2/Properties/Resources.Designer.cs
+++ WpfApplication2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SimpleWebCenter.Properties {
+namespace Calculator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SimpleWebCenter.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleWebCenter.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Calculator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WpfApplication2/Properties/Settings.Designer.cs
+++ WpfApplication2/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SimpleWebCenter.Properties {
+namespace Calculator.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
